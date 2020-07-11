c2a226b0ea185f11dd2a67b62c128c70889896ec
mikhailshepelev <romanist-mike@yandex.ru>
2020-07-11 14:46:47 +0300

    1 commit

--- src/main/java/ee/upcourse/trainingmanager/TrainingmanagerApplication.java
+++ src/main/java/ee/upcourse/trainingmanager/TrainingmanagerApplication.java
@@ -8,6 +8,7 @@ public class TrainingmanagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TrainingmanagerApplication.class, args);
+        System.out.println("sss");
     }
 
 }
