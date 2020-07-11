2f4386f0b3c243c9963ce7582f6d59872e0606cf
Eeva <eevalr@gmail.com>
2020-07-11 14:34:45 +0300

    Initial commit

---
+++ src/main/java/ee/upcourse/trainingmanager/TrainingmanagerApplication.java
@@ -0,0 +1,13 @@
+package ee.upcourse.trainingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingmanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ee/upcourse/trainingmanager/TrainingmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.upcourse.trainingmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
