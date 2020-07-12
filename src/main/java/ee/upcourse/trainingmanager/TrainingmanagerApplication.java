b63f294c32a94bdc0b4fc5219cac28585d69b42d
Ilya Garkusha <ilja.garkusha94@gmail.com>
2020-07-12 11:51:58 +0300

    -modified entities User, Course, Lesson -created entities Topic, Role

--- src/main/java/ee/upcourse/trainingmanager/TrainingmanagerApplication.java
+++ src/main/java/ee/upcourse/trainingmanager/TrainingmanagerApplication.java
@@ -8,6 +8,5 @@ public class TrainingmanagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TrainingmanagerApplication.class, args);
-        System.out.println("sss");
     }
 }
