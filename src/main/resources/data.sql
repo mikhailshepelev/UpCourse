5c5490a4f85aa350333454d19573613aec14bcaf
Ilya Garkusha <ilja.garkusha94@gmail.com>
2020-08-01 15:05:16 +0300

    -updated data.sql like in master

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,40 +1,40 @@
 INSERT INTO user
 VALUES
-(1,  true,'alex@icloud.com', 'Alex', 'Johnson', '1234', 'alexj'),
-(2,  true,'john@gmail.com', 'John', 'Smith', '3453', 'johns'),
-(3,  false,'maria@gmail.com', 'Pamela', 'Kendall', '4321', 'pamk'),
-(4,  true, 'mark@gmail.com', 'Mark', 'Won', '1622', 'markw'),
-(5,  false,'henry@hotmail.com','Henry', 'Cavill','1212','henryc'),
-(6,  true, 'jack@yahoo.com', 'Jack', 'Ryan','1223','jackr'),
-(7,  true,'meena@yahoo.com','Meena','Pradhan','1675','meenap'),
-(8,  true, 'zenab@hotmail.com','Rahman','Shams', '6566','rahmas'),
-(9,  false, 'derek@hotmail.com','Derek','Brian', '1331', 'derekb'),
-(10, true, 'lina@gmail.com','Lina','Maria', '8776','linam'),
-(11, true, 'riko@yahoo.com','Riko','Cecilio', '5454','riko'),
-(12, false,'denis@yahoo.com','Deniss','Jacko', '3668','denisj1234'),
-(13, true, 'christopher@yahoo.com', 'Christopher', 'Nolan','9089','chrisn'),
-(14, true, 'zilot@gmail.com', 'Zilot','Faridi','7687','zilotf'),
-(15, false,'jasper@gmail.com','Ian','Jasper','8997','jasi'),
-(16, true,'marika@gmail.com', 'Marika','Mostalika','6890','marika'),
-(17, false,'mosh@yahoo,com','Mosh','Malik','5566','moshmaa1'),
-(18, true, 'ismail@gmail.com','Ismail','Shroff','7889','ismails'),
-(19, true, 'ajit@yahoo.com', 'Ajit','Malik','1222','ajitm'),
-(20, false,'fria@yahoo.com', 'Fria','Brown','7886','friab'),
-(21, false,'xylo@hotmail.com','Xylo','Wang','8789','xylops'),
-(22, true,'nazaz@yahoo.com','Nazaz','Mikail','1212','nazazm'),
-(23, true,'danny@yahoo.com','Danny','Denzongpa','1546','dannyd'),
-(24, false,'tiger@yahoo.com','Tiger','Shroff','1133','tigers'),
-(25, true,'jackie@gmail.com','Jackie','Shroff','1200','jacky'),
-(26, false,'robert@gmail.com','Robert','Drowney','1300','robertd'),
-(27, true,'chrise@yahoo.com','Chris','Ewans','1450','chrise'),
-(28, false,'ewan@gmail.com','Ewan','McGeor','1100','ewanm1212'),
-(29, true,'daniel@gmail.com','Daniel','craig','0077','daniel007'),
-(30, true,'bond@yahoo.com','James','Bond','1007','james#007'),
-(31, false,'iron@gmail.com','Iron','Man','1333','oronm@1223'),
-(32, true,'webm@gmail.com','Mark','Web','1234','markw'),
-(33, false,'mowgli@yahoo.com','Mowgli','Jen','1999','mowglij'),
-(34, true,'thanos@yahoo.com','Thanos','Alpha','1888','thanos1212'),
-(35, true,'avenger@gmail.com','Avia','Marika','1981','avia1256');
+(1,  true,'alex@icloud.com', 'Alex', 'Johnson', '$2y$12$8uAjEFoHS2PKNOPihJlXN.FEdfewDnv.cDyXAgpSBuHCGhfwXBPOm', 'alexj'),
+(2,  true,'john@gmail.com', 'John', 'Smith', '$2y$12$KXlqaRzRJg5CPlS.8YGC5uXxCGXqCjv3Jc21mGsv9.vQZ.I6On3FG', 'johns'),
+(3,  false,'maria@gmail.com', 'Pamela', 'Kendall', '$2y$12$SRqpBKZvt.5IzTvzXsKSaePtF.YyeejlFXWoP5YHyfjuiWh/DMqFC', 'pamk'),
+(4,  true, 'mark@gmail.com', 'Mark', 'Won', '$2y$12$UmLps77K2fd5/L1xeNfaa.0CcexTrfVowIREfaLmGIkwvbbIpvGZG', 'markw'),
+(5,  false,'henry@hotmail.com','Henry', 'Cavill','$2y$12$Kk0by.Y4ON7r1q7W3LBO8OxNrIIIDr9A1qLnbqPdrOXCU9n/13uKS','henryc'),
+(6,  true, 'jack@yahoo.com', 'Jack', 'Ryan','$2y$12$MnWYQ1W7XwY3IArzaL3W9uXPFl8tIWuUctqyg.0GDC7kwdiFqHdkm','jackr'),
+(7,  true,'meena@yahoo.com','Meena','Pradhan','$2y$12$Lmg8l4zfJXECb1Cl4pK96uGMUL6VBPYmUBqJ2ZnoJsTHv.ahl2e72','meenap'),
+(8,  true, 'zenab@hotmail.com','Rahman','Shams', '$2y$12$0VmTrq5gLDI6zYZMoBQkO.f5WMbFck/26UMKyD.pa/dMWYP.qAHzS','rahmas'),
+(9,  false, 'derek@hotmail.com','Derek','Brian', '$2y$12$1r5INtVjk7hRcIuHVwpnp.R.ReaK22VafIkDmH/cJsSCjCcC4CUOG', 'derekb'),
+(10, true, 'lina@gmail.com','Lina','Maria', '$2y$12$Z3uZrPQk3niNm3RP4CFJJOvfSxdkZN4B4Z1ugUQ81987RxEX0x.2W','linam'),
+(11, true, 'riko@yahoo.com','Riko','Cecilio', '$2y$12$2wv6mzx0N6CafwcDuvCdluDDjlP1QoJCdNwOSzWo1HD880BfJf.Oq','riko'),
+(12, false,'denis@yahoo.com','Deniss','Jacko', '$2y$12$k4ieRMXCiBm26cs4sqODy.0dXZLQACO6aIrZMLgu1JVXsplmgcbT2','denisj1234'),
+(13, true, 'christopher@yahoo.com', 'Christopher', 'Nolan','$2y$12$XuQdV6w8EnHdNvT1crdpeu2aDjaPcov.bbtSGN3Jrd06Z6S8rLPoe','chrisn'),
+(14, true, 'zilot@gmail.com', 'Zilot','Faridi','$2y$12$Gr46H3kaBv1/UYRQlMZI1uRaza7GrmTgKEyh8bdMlNEecrprrrVda','zilotf'),
+(15, false,'jasper@gmail.com','Ian','Jasper','$2y$12$LZaUaPg9IjOi5iAA3owHS.sQtY7jqf4h0qUtaR.m8CPi2ov/C.Ztu','jasi'),
+(16, true,'marika@gmail.com', 'Marika','Mostalika','$2y$12$/g4VnyeRN0Rd4dkxuXjGCuG5KeGPkkxqF6cDnpBLoTgcYmgBjp7NS','marika'),
+(17, false,'mosh@yahoo,com','Mosh','Malik','$2y$12$0FEshvto180aH5IkDrBDUOAFR17uXq9uoBG8o6Q83r4sjSFjOrypW','moshmaa1'),
+(18, true, 'ismail@gmail.com','Ismail','Shroff','$2y$12$j7yzBp6QFz/.4zovjeqWK./PNy7eXI6jNpqqlnTuwjt6t9AtP2TyS','ismails'),
+(19, true, 'ajit@yahoo.com', 'Ajit','Malik','$2y$12$eSwFbGJmys4F2XzxSSHdxO6Q4fU0QwCyBYdqFr7nQwRcHSya6KZvS','ajitm'),
+(20, false,'fria@yahoo.com', 'Fria','Brown','$2y$12$HbEiXWyTOIssNorzZnucjO0M6.zin2jOd6j26XRlsrzXeG.gOtvgu','friab'),
+(21, false,'xylo@hotmail.com','Xylo','Wang','$2y$12$RS4a7ZV3D9EhshFQEPxpeukvAofFEft2CVI/D4R6YMkHXLiboaTnC','xylops'),
+(22, true,'nazaz@yahoo.com','Nazaz','Mikail','$2y$12$fTpTRRnEH8nZIpLQya3TE.MgP6a1DvY1H0Vv3GWS7dXm/CjHvqFtG','nazazm'),
+(23, true,'danny@yahoo.com','Danny','Denzongpa','$2y$12$.7rD7Wjqt0X/Mm1B9yJ04Of5aCvjpwW.dc7ykR88h3I//z3Hd7BPK','dannyd'),
+(24, false,'tiger@yahoo.com','Tiger','Shroff','$2y$12$KxSHk2o7c1ZOikeCtGZbsOWdkQ7MAV2ZYNaxgamEX0rinANFq7Kha','tigers'),
+(25, true,'jackie@gmail.com','Jackie','Shroff','$2y$12$.luipwrFnzupz9yjSnyzjexV1mmT2r1omZIOhevppaO4SUv3BKyKO','jacky'),
+(26, false,'robert@gmail.com','Robert','Drowney','$2y$12$joiI/wdOgqJOl8qz21vUb.xEPWQWERyTcxrrRZJiH7AEwqFgv4j5G','robertd'),
+(27, true,'chrise@yahoo.com','Chris','Ewans','$2y$12$Dgkp4/2sGKNDxKPiV/HZAurEX1EVf2r/0LBM11YlKSI.nc379lGZi','chrise'),
+(28, false,'ewan@gmail.com','Ewan','McGeor','$2y$12$zZz6G1/JMIW.l7SaspuzkuadE27PhYoCgNjs6twoV/sCSGgjH71Tm','ewanm1212'),
+(29, true,'daniel@gmail.com','Daniel','craig','$2y$12$MbbaSmpcMbg91sgRTF/XT.yw4LCv8aEaP6vWE0pLIcAf8HVNXNoem','daniel007'),
+(30, true,'bond@yahoo.com','James','Bond','$2y$12$qJA6dEMG2tuhYwJF.vgMz.B4BW3wpL2OlhMmooQrRQFld0jrOJJfW','james#007'),
+(31, false,'iron@gmail.com','Iron','Man','$2y$12$LjpN/4ArVKOTaC05SilplubKzyPjO7oK/i9q5Di0rAvt1304BXrCa','oronm@1223'),
+(32, true,'webm@gmail.com','Mark','Web','$2y$12$G44vVLs/uT1hrIkwSEkooeyWtEOtTAgCRcLdG6UUWnJLtJRrCE78O','markw'),
+(33, false,'mowgli@yahoo.com','Mowgli','Jen','$2y$12$MLHdz3x/HKgR1hYcsRMC2eI.X/HEe1NOHbm31XRkGSrH8oypJnO6K','mowglij'),
+(34, true,'thanos@yahoo.com','Thanos','Alpha','$2y$12$Rvt5HuX2RxS.mkKzFKwhwedQInxOFoUG3oiYkVuImT8rBBBmKj8my','thanos1212'),
+(35, true,'avenger@gmail.com','Avia','Marika','$2y$12$KJ.8Jt7csuuiT8z0bkazNOiNCXOg0JnDbPdywOwaBD.yMz5J8DmFy','avia1256');
 
 INSERT INTO role
 VALUES
@@ -44,19 +44,19 @@ VALUES
 
 INSERT INTO course
 VALUES
-(2, 'Java Python 2'),
-(3, 'Java Tallinn 9'),
-(4, 'Java Testing 5'),
-(5, 'Java Python 3'),
-(6, 'Java Python 4'),
-(7, 'Java Python 5'),
-(8, 'Java Python 6'),
-(9, 'Java Python 7'),
-(10, 'Java Tallinn 10'),
-(12, 'Java Tallinn 11'),
-(13, 'Java Tallinn 12'),
-(14, 'Java Tallinn 13'),
-(15, 'Java Tallinn 14'),
+(2, 'Java for Beginners 1'),
+(3, 'Python 2'),
+(4, 'Java Advanced 5'),
+(5, 'Python 3'),
+(6, 'Python 4'),
+(7, 'Python 5'),
+(8, 'Python 6'),
+(9, 'Python 7'),
+(10, 'Java for Beginners 10'),
+(12, 'Java for Beginners 11'),
+(13, 'Java for Beginners 12'),
+(14, 'Java for Beginners 13'),
+(15, 'Java for Beginners 14'),
 (17, 'Software Testing 2'),
 (18, 'Software Testing 3'),
 (19, 'Software Testing 4'),
@@ -65,16 +65,15 @@ VALUES
 (22, 'JavaScript 2'),
 (23, 'JavaScript 3'),
 (24, 'JavaScript 4'),
-(26, 'Advance Software Testing 1'),
-(27, 'Advance Software Testing 2'),
-(28, 'Advance Software Testing 3'),
-(29, 'Advance Software Testing 4'),
-(30, 'Advance Software Testing 5'),
-(32, 'Java Advance 1'),
-(33, 'Java Advance 2'),
-(34, 'Java Advance 3'),
-(35, 'Java Advance 4');
-
+(26, 'Advanced Software Testing 1'),
+(27, 'Advanced Software Testing 2'),
+(28, 'Advanced Software Testing 3'),
+(29, 'Advanced Software Testing 4'),
+(30, 'Advanced Software Testing 5'),
+(32, 'Java Advanced 1'),
+(33, 'Java Advanced 2'),
+(34, 'Java Advanced 3'),
+(35, 'Java Advanced 4');
 
 INSERT INTO user_roles
 VALUES
@@ -116,7 +115,6 @@ VALUES
 
 INSERT INTO course_user
 VALUES
-
 (1,2),
 (1,3),
 (1,4),
@@ -148,16 +146,23 @@ VALUES
 (5,34),
 (5,35);
 
-
 INSERT INTO topic
 VALUES
-(1, 'Java Fundamentals', 2, 10),
-(2, 'Java Advanced', 3, 10),
-(3, 'Java Securities', 4, 32),
-(4, 'Python Fundamentals', 5, 24);
-
+(1, 'Java Fundamentals', 2),
+(2, 'Python Fundamentals', 3),
+(3, 'JUnit', 17),
+(4, 'Python Fundamentals', 5),
+(5, 'Spring Framework', 4);
 
 INSERT INTO lesson
 VALUES
-(1, '2020-10-15', 'Java Fundamentals', 1);
-
+(1, '2020-10-15', 'Introduction to Java', 1),
+(2, '2020-10-18', 'Hello world project', 1),
+(3, '2020-10-24', 'Data types and variables', 1),
+(4, '2020-10-25', 'Statements in Java', 1),
+(5, '2020-11-01', 'Operators, Operands and Expressions', 1),
+(6, '2020-11-01', 'Spring Overview', 5),
+(7, '2020-11-11', 'Spring XML Configuration', 5),
+(8, '2020-11-12', 'Spring Configuration with Java Annotations', 5),
+(9, '2020-11-19', 'Spring MVC', 5),
+(10, '2020-11-25', 'Spring Security', 5)
